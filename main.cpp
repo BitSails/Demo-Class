6f594a66765ef1a75d44808f96d02f5aad0e86e7
NishanthMolleti <48158364+NishanthMolleti@users.noreply.github.com>
2019-10-09 14:58:23 +0530

    updated main function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 
 #include <iostream>
 using namespace std;
-
+class Demo
+{
+  public:
+   void fun()
+   {
+     cout<<"Class has been implemented";
+   }
+};
 
 int main()
 {
-
+   Demo o;
+  o.fun;
 }
